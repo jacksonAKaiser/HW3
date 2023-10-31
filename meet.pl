22fb0fef91ea0fe2417ea5ab6561617d18555ee6
Jackson Kaiser <142761347+jacksonAKaiser@users.noreply.github.com>
2023-10-30 19:20:17 -0600

    Working Code?

--- meet.pl
+++ meet.pl
@@ -3,9 +3,20 @@
 :- include('data.pl').
 :- include('uniq.pl').
 
-% Your code goes here.
-
 people([ann,bob,carla]).
+
+% Define the meet predicate to find common slots
+meet(Slot) :-
+  people(People),
+  fine_common_slot(People, Slot).
+
+% Define a recursive predicate to find common slot
+find_common_slot([Person|[]], Slot) :-
+  free(Person, Slot).
+
+find_common_slot([Person|Rest], Slot) :-
+  free(Person, Slot),
+  find_common_slot(Rest, Slot).
 
 main :- findall(Slot, meet(Slot), Slots),
         uniq(Slots, Uniq),
